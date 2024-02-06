f2e13d63dce1725ffb99089edccb10312daca09d
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-02-06 09:24:08 +0100

    rework crawl function

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const { Pool } = require("pg");
 const PORT = process.env.PORT || 3001;
 // use .env file to store sensitive data
 require("dotenv").config();
+const { nextSevenDays, splitCinemaNameAtDate } = require("./lib/utils.js");
+const { listOfCinemas } = require("./lib/hardcodedData.js");
 
 app.use(cors());
 app.use(express.json());
@@ -18,19 +20,6 @@ const pool = new Pool({
   port: 5432,
 });
 
-const listOfCinemas = [
-  {
-    URL: "https://www.yorck.de/en/films?sort=Popularity&date=2021-09-17&tab=daily&sessionsExpanded",
-    name: "Yorck Kinos",
-    titleFieldClass: "h3.whatson-film-title",
-  },
-  {
-    URL: "https://www.hoefekino.de/programm-tickets/vorschau/woche/",
-    name: "Hackesche Höfe Kino",
-    titleFieldClass: "h2",
-  },
-];
-
 app.get("/crawl", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
@@ -38,44 +27,42 @@ app.get("/crawl", async (req, res) => {
       args: ["--disable-features=site-per-process"],
     });
     const page = await browser.newPage();
-
-    // get every day of the next week formatted as YYYY-MM-DD
-    const dates = Array.from({ length: 7 }, (_, i) => {
-      const date = new Date();
-      date.setDate(date.getDate() + i);
-      return date.toISOString().split("T")[0];
-    });
     const movieTitles = [];
 
     for (const cinema in listOfCinemas) {
-      const dateRegex = /\d{4}-\d{2}-\d{2}/;
-      const splitURL = cinema.URL.split(dateRegex);
-      for (const date of dates) {
+      console.log("cinema", cinema);
+      const splitURL = splitCinemaNameAtDate(listOfCinemas[cinema].urlString);
+      for (const date of nextSevenDays) {
         await page.goto(`${splitURL[0]}${date}${splitURL[1]}`);
 
-        await Promise.all(
-          page
-            .evaluate(() => {
-              const titleElements = document.querySelectorAll(
-                cinema.titleFieldClass
-              );
-              const titles = Array.from(titleElements).map((titleElement) =>
-                titleElement.textContent.trim()
-              );
-              return titles;
-            })
-            .then((titles) => {
-              movieTitles.push(titles);
-            })
-            .catch((error) => {
-              console.error("Error during crawling:", error);
-              res.status(500).json({ error: "Internal Server Error" });
-            })
+        movieTitles.push(
+          page.evaluate(() => {
+            const titleElements = document.querySelectorAll(
+              listOfCinemas[cinema].titleFieldClass
+            );
+            const titles = titleElements.map((title) => title.innerText);
+            return titles;
+          })
         );
-      }
 
-      // const movieTitlesFlat = movieTitles.flat();
-      // const movies = Array.from(new Set(movieTitlesFlat));
+        // await Promise.all(
+        //   page
+        //     .evaluate(() => {
+        //       const titleElements = document.querySelectorAll(
+        //         listOfCinemas[cinema].titleFieldClass
+        //       );
+        //       const titles = titleElements.map((title) => title.innerText);
+        //       return titles;
+        //     })
+        //     .then((titles) => {
+        //       movieTitles.push(titles);
+        //     })
+        //     .catch((error) => {
+        //       console.error("Error during crawling:", error);
+        //       res.status(500).json({ error: "Internal Server Error" });
+        //     })
+        // );
+      }
 
       console.log(movieTitles);
     }
