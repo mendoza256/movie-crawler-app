34e1e2b361f4493db1c6e363a5e3694b453c6cc2
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-02-13 17:58:39 +0100

    add mongodb, mongoose - remove docker files - add auth routes - add auth controllers - add models

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const { postLogin } = require("../controllers/auth");
+const User = require("../models/user");
+const { login, register, getUser, logout } = require("../controllers/auth");
 
 router.use(cors());
+
+router.post("/login", login);
+
+router.post("/register", register);
+
+router.get("/user", getUser);
 
 router.get("/", (req, res) => {
   res.send("Auth route");
 });
 
-router.post("/login", postLogin);
+router.get("/logout", logout);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,40 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const session = require("express-session");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const mongodbUri =
+  "mongodb+srv://christian:MM8xTBxHCwp186t9@cluster0.5uqboef.mongodb.net/movie-crawler?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongodbUri)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
+
 const crawlerRoutes = require("./routes/crawler");
 const authRoutes = require("./routes/auth");
-const pool = require("./db");
+
 const PORT = process.env.PORT || 3001;
 
 require("dotenv").config();
 
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(
   session({ secret: "my-secret", resave: false, saveUninitialized: false })
 );
-
-app.get("/", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM crawl_data");
-    res.status(200).send(data.rows);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-app.get("/setup", async (req, res) => {
-  try {
-    await pool.query(
-      "Create table if not exists crawl_data (id serial primary key, url varchar(255), title varchar(255), cinema varchar(255), created_at timestamp default current_timestamp)"
-    );
-    res.status(200).json({ message: "Table created" });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
 
 app.use("/crawl", crawlerRoutes);
 app.use("/auth", authRoutes);
